785eb16bd5218c3f428fef89a56186364bf201b8
Cth103 <ItsAMeCth@gmail.com>
2016-12-06 23:21:30 +0100

    wen eta

---
+++ igbaos/dedi.js
@@ -0,0 +1,16 @@
+
+function setdedi(nom) {
+    document.getElementById("dedis").innerHTML = "<h3>"+nom+"</h3>";
+}
+setdedi("LoftyNine7");
+
+window.setInterval(function () {
+setTimeout(function(){ setdedi("FireC"); }, 3000);
+setTimeout(function(){ setdedi("JeuxJeux20 (RPZ le 63 et 95)"); }, 6000);
+setTimeout(function(){ setdedi("S0n1c"); }, 9000);
+setTimeout(function(){ setdedi("TTie"); }, 12000);
+setTimeout(function(){ setdedi("Da532"); }, 15000);
+setTimeout(function(){ setdedi("myself (cth103)"); }, 20000);
+setTimeout(function(){ setdedi("Again,"); }, 24000);
+    }, 26000); 
+
---
+++ igbaos/my-app.js
@@ -0,0 +1,11 @@
+var iGBAOS = new Framework7({
+    modalTitle: 'igba os'
+});
+
+var auto_updates = true;
+
+var $$ = Dom7;
+
+var homeView = iGBAOS.addView('#home', {
+    dynamicNavbar: true
+});
